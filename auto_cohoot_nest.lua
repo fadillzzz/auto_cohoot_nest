b152f2ae15e3dd1023090067e47f37fb7da4a455
fadillzzz <fadillzzz@gmail.com>
2022-08-25 15:39:58 +0700

    feat(nest): Auto loot both nests

--- auto_cohoot_nest.lua
+++ auto_cohoot_nest.lua
@@ -6,39 +6,49 @@ local settings = {
 	maxStock = 5;
 }
 
-local elgadoMap = {[6]=true}
-local kamuraMap = {[0]=true, [1]=true, [2]=true, [3]=true, [4]=true, [7]=true}
+local elgadoMap = {[6]=true, [7]=true}
+local kamuraMap = {[0]=true, [1]=true, [2]=true, [3]=true, [4]=true}
 
-function autoPickNest(retval)
+function autoPickNest()
 	if not settings.enable then
-		return
+		return nil
 	end
-	
-	local villageAreaManager = sdk.get_managed_singleton("snow.VillageAreaManager")
-	
-	if not villageAreaManager then
-		return
-	end
-	
-	local villageNum = villageAreaManager:call("get__CurrentAreaNo")
-	
-	local owlNestManagerSingleton = sdk.get_managed_singleton("snow.progress.ProgressOwlNestManager")
-	local progressOwlNestSaveData = owlNestManagerSingleton:get_SaveData()
 
-	if not owlNestManagerSingleton or not progressOwlNestSaveData then
-		return
+	local villageAreaManager = sdk.get_managed_singleton("snow.VillageAreaManager")
+
+	if not villageAreaManager then
+		return nil
 	end
-	
-	local owlNestCount = 0
-	if elgadoMap[villageNum] then
-		owlNestCount = progressOwlNestSaveData:get_field("_StackCount2")
-	elseif kamuraMap[villageNum] then
-		owlNestCount = progressOwlNestSaveData:get_field("_StackCount")
-	end
-	
-	if owlNestCount >= settings.maxStock and owlNestManagerSingleton then
+
+	local villageNum = villageAreaManager:call("get__CurrentAreaNo")
+
+	local owlNestManagerType = sdk.find_type_definition("snow.progress.ProgressOwlNestManager")
+	local getSaveDataMethod = owlNestManagerType:get_method("get_SaveData")
+	local stack_item_count_field = owlNestManagerType:get_field("StackItemCount")
+
+	local owlNestSaveData = sdk.find_type_definition("snow.progress.ProgressOwlNestSaveData")
+	local stackCount1Field = owlNestSaveData:get_field("_StackCount")
+	local stackCount2Field = owlNestSaveData:get_field("_StackCount2")
+
+	local ID1 = owlNestSaveData:get_field("_StackItemID")
+	local ID2 = owlNestSaveData:get_field("_StackItemID2")
+
+	local owlNestManagerSingleton = sdk.get_managed_singleton("snow.progress.ProgressOwlNestManager")
+	local owlNestSaveData = getSaveDataMethod:call(owlNestManagerSingleton)
+
+	local kamuraCount = stackCount1Field:get_data(owlNestSaveData)
+	local elgadoCount = stackCount2Field:get_data(owlNestSaveData)
+
+	if kamuraCount >= settings.maxStock then
+		villageAreaManager:call("set__CurrentAreaNo", 2)
 		owlNestManagerSingleton:supply()
 	end
+
+	if elgadoCount >= settings.maxStock then
+		villageAreaManager:call("set__CurrentAreaNo", 6)
+		owlNestManagerSingleton:supply()
+	end
+
 end
 
 local function SaveSettings()
@@ -58,12 +68,12 @@ end
 re.on_draw_ui(function()
 	local changed = false;
 
-    if imgui.tree_node("Auto cohoot nest") then
+	if imgui.tree_node("Auto cohoot nest") then
 
 		changed, settings.enable = imgui.checkbox("Enabled", settings.enable);
 		changed, settings.maxStock = imgui.slider_int("Maximum stock", settings.maxStock, 1, 5);
 		imgui.tree_pop()
-    end
+	end
 end)
 
 re.on_config_save(function()
